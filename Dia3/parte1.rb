990b26d4f94fc9e517cd93035f2f10e581116227
Kukuy <diego.salazar.p@gmail.com>
2023-12-12 02:02:52 -0300

    Terminado, pero podria mejorar

--- Dia3/parte1.rb
+++ Dia3/parte1.rb
@@ -1,4 +1,5 @@
-file = File.readlines('./Dia3/test.txt').map(&:strip)
+# file = File.readlines('./Dia3/test.txt').map(&:strip)
+file = File.readlines('./Dia3/input.txt').map(&:strip)
 
 def findNumCoords(file, i, j)
     left, right = j,j
@@ -7,44 +8,45 @@ def findNumCoords(file, i, j)
         left -= 1
     end
     #derecha
-    while (file[i][right+1].match?(/\d/))
+    while (right < file[i].length-1 and file[i][right+1].match?(/\d/))
         right += 1
     end
-    p [i,j], left, right
+    # p [i,j], left, right
     p file[i][left..right]
     return [i,left,right]
 end
 
 def adyacentes(file, i,j)
     pos = [-1, 0, 1].repeated_permutation(2).to_a.select{|a| a != [0,0]}
-    pos.each do |xy|
-        x,y = xy
+    pos.each do |pair|
+        x,y = pair
         if (i + x) < 0 or (i + x) > file.length-1 or (j + y) < 0 or (j + y) > file[i].length-1
             next
         end
         #si es digito, encotrar coordenas de los digitos
+        pp [file[i + x][j + y], i,j, pair]
         if file[i + x][j + y].match?(/\d/)
-            # puts file[i + x][j + y]
+            # pp file[i + x][j + y], findNumCoords(file,i+x,j+y)
             $num_coords.append(findNumCoords(file,i+x,j+y))
         end
     end
 end
 
-# p findNumCoords(file,0,5)
-
-# pp file
 
 $num_coords = []
+# p file[0].length
 file.each_with_index do |line,i|
-    line.each_char.with_index do |c,j| 
-        if c.match?(/[\*\@\#\+\=\\\/\$\%]/)
-        #Falta ver que hacer 
+    line.each_char.with_index do |c,j|
+        # pp [i,j]
+        if c.match?(/[\*\@\#\+\=\\\/\$\%\&\-\_]/)
             adyacentes(file,i,j)
         end 
 
     end
 end
+
 total = 0
+
 $num_coords.uniq.each do |coord|
     i,left,right = coord
     total += file[i][left..right].to_i
