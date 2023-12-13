6bb29970d6960352a4a0aef5321370e08fbd9a61
Kukuy <diego.salazar.p@gmail.com>
2023-12-13 17:55:20 -0300

    done parte1

--- Dia5/parte1.rb
+++ Dia5/parte1.rb
@@ -1,20 +1,18 @@
-file = File.readlines("./Dia5/test.txt").map(&:strip)
+# file = File.readlines("./Dia5/test.txt").map(&:strip)
+file = File.readlines("./Dia5/input.txt").map(&:strip)
 
 seeds = file.slice!(0,2)[0].split(":")[1].split().map(&:to_i)
+
 maps = file.chunk { |element| element != "" }.select(&:first) \
-    .map(&:last).map{|a|a.slice(1..-1).map(&:split).map{|p| p.map(&:to_i)}}
+    .map(&:last).map{|a| a.slice(1..-1).map(&:split).map{|p| p.map(&:to_i)}}
 
-maps.map!{|el| el.map{|m| {m[1]..m[1]+m[2]-1 => m[0]..m[0]+m[2]-1 }}}
-
-# p maps
+pp maps
 
 def inOut(seed, maps)
-    maps.each do |pair|
-        rang = pair.keys.first
-        if rang.include? seed
-            i = rang.to_a.index seed
-            # pp [seed, rang.size, i, pair.values.first.size]
-            return pair.values.first.to_a[i]
+    maps.each do |trio|
+        dest, origin, ratio = trio
+        if seed >= origin and seed < origin + ratio
+            return seed + (dest-origin)
         end
     end
     return seed
