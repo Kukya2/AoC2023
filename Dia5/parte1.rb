b14cdb3c4bc57e5970374e1c6c697ce684436cb0
Kukuy <diego.salazar.p@gmail.com>
2023-12-12 22:33:47 -0300

    update functions

--- Dia5/parte1.rb
+++ Dia5/parte1.rb
@@ -1,7 +1,8 @@
 file = File.readlines("./Dia5/test.txt").map(&:strip)
 
 seeds = file.slice!(0,2)[0].split(":")[1].split().map(&:to_i)
-maps = file
+maps = file.chunk { |element| element != "" }.select(&:first) \
+    .map(&:last).map{|a|a.slice(1..-1).map(&:split).map{|p| p.map(&:to_i)}}
 
 pp seeds
 pp maps
