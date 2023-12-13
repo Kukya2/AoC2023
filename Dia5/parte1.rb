aef382dabfa07fc0cf14568331b68f37a4683162
Kukuy <diego.salazar.p@gmail.com>
2023-12-13 17:32:36 -0300

    fix bug

--- Dia5/parte1.rb
+++ Dia5/parte1.rb
@@ -4,17 +4,25 @@ seeds = file.slice!(0,2)[0].split(":")[1].split().map(&:to_i)
 maps = file.chunk { |element| element != "" }.select(&:first) \
     .map(&:last).map{|a|a.slice(1..-1).map(&:split).map{|p| p.map(&:to_i)}}
 
+maps.map!{|el| el.map{|m| {m[1]..m[1]+m[2]-1 => m[0]..m[0]+m[2]-1 }}}
+
+# p maps
+
 def inOut(seed, maps)
-    ranges = maps.map{|m| {m[1]..m[1]+m[2]-1 => m[0]...m[0]+m[2]-1 }}
-    p ranges
-    ranges.each do |pair|
+    maps.each do |pair|
         rang = pair.keys.first
         if rang.include? seed
-           i = rang.to_a.index seed
-           return pair.values.first.to_a[i]
+            i = rang.to_a.index seed
+            # pp [seed, rang.size, i, pair.values.first.size]
+            return pair.values.first.to_a[i]
         end
     end
+    return seed
 end
 
-pp seeds
-pp inOut(seeds[0],maps[0])
+# seeds.each{|s| p inOut(s,maps[0])}
+
+maps.each do |mapas|
+    seeds.map!{|s| inOut(s, mapas)}
+end
+p seeds.min
