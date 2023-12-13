d7be7f38eaf432e811fc2d747028993166471aeb
Kukuy <diego.salazar.p@gmail.com>
2023-12-13 16:03:26 -0300

    make inOut func

--- Dia5/parte1.rb
+++ Dia5/parte1.rb
@@ -4,5 +4,17 @@ seeds = file.slice!(0,2)[0].split(":")[1].split().map(&:to_i)
 maps = file.chunk { |element| element != "" }.select(&:first) \
     .map(&:last).map{|a|a.slice(1..-1).map(&:split).map{|p| p.map(&:to_i)}}
 
+def inOut(seed, maps)
+    ranges = maps.map{|m| {m[1]..m[1]+m[2]-1 => m[0]...m[0]+m[2]-1 }}
+    p ranges
+    ranges.each do |pair|
+        rang = pair.keys.first
+        if rang.include? seed
+           i = rang.to_a.index seed
+           return pair.values.first.to_a[i]
+        end
+    end
+end
+
 pp seeds
-pp maps
+pp inOut(seeds[0],maps[0])
