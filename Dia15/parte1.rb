9d0f9f367e8bbee209e83998115adf81f08cdbf0
Kukuy <diego.salazar.p@gmail.com>
2023-12-15 18:31:51 -0300

    Done parte 1

--- Dia15/parte1.rb
+++ Dia15/parte1.rb
@@ -1,16 +1,20 @@
-file = File.new("./Dia15/test.txt").readline.split ","
+# file = File.new("./Dia15/test.txt").readline.split ","
+file = File.new("./Dia15/input.txt").readline.split ","
 
 
 def getHash(str)
     count = 0
     str.each_char do |chr|
-        # p chr
         count += chr.ord
         count *= 17
-        count = count % 256
-        # p count
+        count %=  256
     end
     return count
 end
 
-pp getHash("HASH")
+total = 0
+file.each do |str|
+    total += getHash(str)
+end
+
+pp total
